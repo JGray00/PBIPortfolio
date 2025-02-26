d2e7e2d5012ac14b2ebc072f364de887373f3314
JGray00 <115090650+JGray00@users.noreply.github.com>
2025-02-26 21:13:33 +0000

    Create LastMonthOrdersTerritory.sql

---
+++ 1. KPI Dashboard/LastMonthOrdersTerritory.sql
@@ -0,0 +1,12 @@
+SELECT top 1 WITH TIES
+Territory,
+FORMAT(OrderDate,'yyyMM') as OrderDate,
+FORMAT(OrderDate,'MMM-yyy') as OrderDate2,
+COUNT(DISTINCT([SalesOrderID])) as TotalOrders
+FROM [Sales].[vSalesSummary]
+
+GROUP BY 
+Territory,
+FORMAT(OrderDate,'yyyMM'),
+FORMAT(OrderDate,'MMM-yyy')
+order by FORMAT(OrderDate,'yyyMM') desc
