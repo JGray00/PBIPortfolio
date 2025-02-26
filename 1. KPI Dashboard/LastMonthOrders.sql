f49163efa92df61efb0a6708ce3fbca8b050390f
JGray00 <115090650+JGray00@users.noreply.github.com>
2025-02-26 21:04:17 +0000

    Create LastMonthOrders.sql

---
+++ 1. KPI Dashboard/LastMonthOrders.sql
@@ -0,0 +1,9 @@
+SELECT top 1
+FORMAT(OrderDate,'yyyMM') as OrderDate,
+FORMAT(OrderDate,'MMM-yyy') as OrderDate2,
+COUNT(DISTINCT([SalesOrderID])) as TotalOrders
+FROM [Sales].[vSalesSummary]
+
+GROUP BY FORMAT(OrderDate,'yyyMM'),
+FORMAT(OrderDate,'MMM-yyy')
+order by FORMAT(OrderDate,'yyyMM') desc
