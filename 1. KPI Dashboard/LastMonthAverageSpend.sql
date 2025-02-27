4639cdcde07d57e9dd907eb629e6db8898563025
JGray00 <115090650+JGray00@users.noreply.github.com>
2025-02-27 16:39:39 +0000

    Update LastMonthAverageSpend.sql

--- 1. KPI Dashboard/LastMonthAverageSpend.sql
+++ 1. KPI Dashboard/LastMonthAverageSpend.sql
@@ -1,7 +1,7 @@
 SELECT
 FORMAT(OrderDate,'yyyMM') as OrderDate,
 FORMAT(OrderDate,'MMM-yyy') as OrderDate2,
-FORMAT(SUM(LineTotal)/COUNT(DISTINCT([SalesOrderID])),'C2') as AverageOrder
+SUM(LineTotal)/COUNT(DISTINCT([SalesOrderID])) as AverageOrder
 FROM [Sales].[vSalesSummary]
 
 GROUP BY 
