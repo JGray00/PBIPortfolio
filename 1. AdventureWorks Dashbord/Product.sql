a6706f71873ce36226e4f2ab6c5bb1418d9044f2
JGray00 <115090650+JGray00@users.noreply.github.com>
2025-04-22 21:03:33 +0100

    Create Product.sql

---
+++ 1. AdventureWorks Dashbord/Product.sql
@@ -0,0 +1,15 @@
+SELECT
+CAST(Prod.ProductID as int) as [ProductID],
+CAST(Prod.Name as varchar) as ProductName,
+CASE 
+	WHEN ProdSub.Name is null THEN CAST('Other' as varchar) 
+	ELSE CAST(ProdSub.Name as varchar)
+END as Subcategory,
+CASE
+	WHEN ProdCat.Name is null THEN CAST('Other' as varchar) 
+	ELSE CAST(ProdCat.Name as varchar)
+END as Subcategory
+
+FROM [Production].[ProductSubcategory] ProdSub
+FULL JOIN Production.[Product] Prod on ProdSub.ProductSubcategoryID = Prod.ProductSubcategoryID
+FULL JOIN Production.ProductCategory ProdCat on ProdCat.ProductCategoryID = ProdSub.ProductCategoryID
